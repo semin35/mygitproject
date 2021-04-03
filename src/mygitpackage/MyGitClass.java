c3833369d7d89fd6afeddda9ca566b41b6eb488a
seray <seraykilinc35@gmail.com>
2021-04-03 19:50:49 +0300

    version 3

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,6 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("my fist commit");
 		System.out.println("my second commit");
+		System.out.println("my third commit");
 
 	}
 

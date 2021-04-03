214d7c4b4b95f06788ac2603ff561bc29e563ebb
seray <seraykilinc35@gmail.com>
2021-04-03 19:43:47 +0300

    version 2

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,7 +3,8 @@ package mygitpackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-		System.out.println("Version 1");
+		System.out.println("my fist commit");
+		System.out.println("my second commit");
 
 	}
 
